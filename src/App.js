98e90b8c3ec4eb677761ab2b1f6df2a7e14bfb92
Leevi <leewi.98@hotmail.com>
2023-08-28 17:28:52 +0300

    Slider for testing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import logo from './norppa.png';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import CountdownTimer from './components/CountdownTimer';
+import Slider from './components/Slider';
 function App() {
+  const [animationSpeed, setAnimationSpeedValue] = useState(1);
+
+  const handleSliderChange = (newValue) => {
+    setAnimationSpeedValue(newValue); // Update the modified value
+  };
   const targetDate = '2023-11-10T00:00:00'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{
+          animationDuration: `${20 / animationSpeed}s`
+          }}/>
         <p>
           Integraatiofest soon<sup>TM</sup>
         </p>
         
       <CountdownTimer targetDate={targetDate}/>
+      <Slider onSliderChange={handleSliderChange} />
       </header>
-      
     </div>
   );
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Slider.css'; // Make sure to create this CSS file
+
+const Slider = ({onSliderChange}) => {
+  const [value, setValue] = useState(1); // Initial value
+
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    setValue(newValue);
+    onSliderChange(newValue);
+  };
+
+  return (
+    <div className="slider-container">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value={value}
+        onChange={handleChange}
+        className="slider"
+      />
+      <span className="slider-value">{value}</span>
+    </div>
+  );
+};
+
+export default Slider;
