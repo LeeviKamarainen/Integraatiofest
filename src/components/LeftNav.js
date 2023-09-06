c7a88005c1b6b4b36e786059244ac18934f961cb
Leevi <leewi.982@hotmail.com>
2023-09-06 12:13:09 +0300

    Fixed left navbar height

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -7,7 +7,7 @@ import { Youtube } from 'react-bootstrap-icons';
 import { Telegram } from 'react-bootstrap-icons';
 function LeftNavBar() {
     return (
-      <Navbar className="bottomnav" expand="lg" fixed="left" bg="none" variant="dark" style={{ width: '10%', left:'0%', top:"10%", height:"100vh", borderRadius: '10px', flexDirection:"column",alignItems:"flex-start",justifyContent:"flex-start"}}>
+      <Navbar className="bottomnav" expand="lg" fixed="left" bg="none" variant="dark" style={{ width: '10%', left:'0%', top:"10%", height:"50vh", borderRadius: '10px', flexDirection:"column",alignItems:"flex-start",justifyContent:"flex-start"}}>
             <Button variant="link" href="https://open.spotify.com/track/7kXPGRvZAoo0J0gzc8hEGu?si=43eeb1b3df5c474f">
               <Spotify style={{color: 'rgb(0, 128, 55)', fontSize:"3rem"}}/>
             </Button>
