df85b4c62a603ae8e384cdede274cdce1a0399be
arnot <arno.toro@gmail.com>
2023-10-01 15:06:27 +0300

    refactored code and removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import CountdownTimer from './components/CountdownTimer';
 import LeftNav from './components/LeftNav'
 import ReactPlayer from 'react-player';
+import TimeTable from './components/Timetable';
 
 import logo from './assets/images/norppa.png';
 import promoVideo from './assets/videos/promoVideo1.mp4';
@@ -47,27 +48,7 @@ function App() {
           <div className="flex-container">
             <div className="flex-wrapper timetable">
               <h1 className="timetable-title">Alustava aikataulu</h1>
-              <ul className="timetable-list">
-                <h3 className="timetable-list-title">Perjantai</h3>
-                <li className="timetable-list-item">17:00 - 18:00 Kaljaa</li>
-                <li className="timetable-list-item">18:00 - 19:00 Illallinen</li>
-                <li className="timetable-list-item">19:00 - 20:00 Sauna</li>
-                <li className="timetable-list-item">20:00 - 04:00 Lisää kaljaa</li>
-
-                <h3 className="timetable-list-title">Lauantai</h3>
-                <li className="timetable-list-item">10:00 - 11:00 Aamupala</li>
-                <li className="timetable-list-item">11:00 - 12:00 Kaljaa</li>
-                <li className="timetable-list-item">12:00 - 13:00 Lounas</li>
-                <li className="timetable-list-item">13:00 - 04:00 Kaljaa</li>
-
-                <h3 className="timetable-list-title">Sunnuntai</h3>
-                <li className="timetable-list-item">10:00 - 11:00 Palju</li>
-                <li className="timetable-list-item">11:00 - 12:00 Sauna</li>
-                <li className="timetable-list-item">12:00 - 13:00 Lounas</li>
-                <li className="timetable-list-item">13:00 - 14:00 Siivous</li>
-                <li className="timetable-list-item">14:00 - 15:00 Kotiinlähtö</li>
-                <li className="timetable-list-item">15:00 - 00:00 Kaljaa</li>
-              </ul>
+              <TimeTable />
             </div>
             <div className="flex-wrapper video">
               <h2 className="video-title">🤫</h2>
