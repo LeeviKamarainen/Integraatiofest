63c9e56000d56f68e9154da01efc6c41b778eb43
arnot <arno.toro@gmail.com>
2023-11-01 12:57:23 +0200

    added short instructions to adding a page to the site and changes schedule font on phones to make it easier to read

---
+++ src/components/PageTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+function PageTemplate() {
+  return (
+    <Box sx={{ flexGrow: 1}} className="content-wrapper <modify this>">
+
+    </Box>
+  )
+}
+
+export default PageTemplate
