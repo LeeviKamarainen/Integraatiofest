3364fa5ee473edab59e6f18d8d763ae024c7aa67
arnotoro <arno.toro@gmail.com>
2023-11-11 14:44:25 +0200

    updated kolmio easter egg

--- src/components/Kolmio.js
+++ src/components/Kolmio.js
@@ -1,62 +1,15 @@
-// ScrollableVideoPage.js
-import React, { useState, useEffect, useRef } from 'react';
-import '../assets/styles/Kolmio.css'; // Import the CSS file for styling
+import React from 'react';
 import kolmio from '../assets/videos/kolmio.mp4';
+import '../assets//styles/Kolmio.css'; // Import the CSS file for styling
 
-const ScrollableVideoPage = () => {
-  const [videos, setVideos] = useState([kolmio]); // Initial set of videos
-  const [shouldLoadMore, setShouldLoadMore] = useState(true); // Control whether more videos can be loaded
-  const videosRef = useRef(null);
-
-  useEffect(() => {
-    const currentVideosRef = videosRef.current; // Create a variable inside the effect
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting && entry.intersectionRatio > 0) {
-            // Add more videos when the last one is fully visible, and shouldLoadMore is true
-            if (shouldLoadMore && videos.length < 501) {
-              setVideos((prevVideos) => [...prevVideos, kolmio]);
-            }
-          } else if (entry.intersectionRatio === 0) {
-            // Set shouldLoadMore to false when the user scrolls away from the last video
-            setShouldLoadMore(false);
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: '200px',
-        threshold: 0.8,
-      }
-    );
-
-    if (currentVideosRef) {
-      observer.observe(currentVideosRef);
-    }
-
-    return () => {
-      if (currentVideosRef) {
-        observer.unobserve(currentVideosRef);
-      }
-    };
-  }, [shouldLoadMore, videos]);
-
+function Kolmio() {
   return (
-    <div className="scrollable-video-page" ref={videosRef}>
-      <div className="video-container">
-        {videos.map((video, index) => (
-          <div key={index} className="video-wrapper">
-            <video autoPlay muted loop width="100%" height="50%">
-              <source src={video} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-        ))}
-      </div>
+    <div className="video-wrapper">
+      <video autoPlay muted loop playsInline className="kolmio-video">
+        <source src={kolmio} type="video/mp4" />
+      </video>
     </div>
   );
-};
+}
 
-export default ScrollableVideoPage;
+export default Kolmio;
